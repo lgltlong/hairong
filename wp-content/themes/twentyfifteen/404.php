1a962bf8d288d7594421a3f8de854a1d9ca2f454
lgltlong <lgltlong@163.com>
2018-11-02 13:46:58 +0800

    完成404页面

--- wp-content/themes/twentyfifteen/404.php
+++ wp-content/themes/twentyfifteen/404.php
@@ -9,22 +9,11 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+<!-- 404 main -->
+<div id="p404" class="container p-0">
+	<div class="cont-lg-head text-center">
+		<img src="<?php imgUrl('404.jpg');?>" alt="<?php echo get_bloginfo('name');?>">
+	</div>
+</div>
 
 <?php get_footer(); ?>
